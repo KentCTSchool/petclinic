e80314476a0873c0623239884842b8f53e4a5728
Kent Pedrocha <101266723@georgebrown.ca>
2021-11-02 21:06:54 -0400

    Added PetClinic Services.Closes #5

---
+++ petclinic/pet-clinic-data/src/main/java/ca/gbc/comp3095/petclinic/services/PetService.java
@@ -0,0 +1,11 @@
+package ca.gbc.comp3095.petclinic.services;
+
+import ca.gbc.comp3095.petclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+    Pet findById(Long id);
+    Pet save(Pet Pet);
+    Set<Pet> findAll();
+}
---
+++ petclinic/pet-clinic-data/src/main/java/ca/gbc/comp3095/petclinic/services/VetService.java
@@ -0,0 +1,11 @@
+package ca.gbc.comp3095.petclinic.services;
+
+import ca.gbc.comp3095.petclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+    Vet findById(Long id);
+    Vet save(Vet vet);
+    Set<Vet> findAll();
+}
